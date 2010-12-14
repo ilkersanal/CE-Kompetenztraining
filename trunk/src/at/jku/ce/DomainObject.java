374c997c5f283778d1df2d6d6d4d5fa391f70ba6
ilker sanal <heondloc@hotmail.com>
2010-12-14 11:54:05 +0100

    2. konstruktor hinzugefügt

--- trunk/src/at/jku/ce/DomainObject.java
+++ trunk/src/at/jku/ce/DomainObject.java
@@ -2,8 +2,16 @@ package at.jku.ce;
 
 public class DomainObject {
 
+public String id;
+
 	public DomainObject() {
 		super();
 	}
 
+	
+	public DomainObject(String id){
+	
+		super();
+		this.id = id;
+	}
 }
