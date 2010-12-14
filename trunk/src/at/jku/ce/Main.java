a79b6cddf34037b830aad39a13256c5fe5ef54a7
geraldkernstock <gerald.kernstock@linzag.net>
2010-12-14 12:02:54 +0100

    Änderungen in Main.java hinzugefügt

--- trunk/src/at/jku/ce/Main.java
+++ trunk/src/at/jku/ce/Main.java
@@ -2,10 +2,25 @@ package at.jku.ce;
 
 public class Main {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		Set<DomainObject> objectSet = new HashSet<DomainObject>();
+
+		objectSet.add(new DomainObject("Helga"));
+		objectSet.add(new DomainObject("Walter"));
+		objectSet.add(new DomainObject("Hannah"));
+		objectSet.add(new DomainObject("Paul"));
+		objectSet.add(new DomainObject("Claudia"));
+
+		for (DomainObject domainObject : objectSet) {
+			LOGGER.debug(domainObject.id);
+		}
+
 	}
 
 }
